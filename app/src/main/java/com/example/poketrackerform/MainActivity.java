77711dc76acff2e38d0fce364722ac949deaaf1e
elisabethhassan <elisabethh.info@gmail.com>
2023-09-21 11:31:40 -0500

    updated interface

--- app/src/main/java/com/example/poketrackerform/MainActivity.java
+++ app/src/main/java/com/example/poketrackerform/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.poketrackerform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nationalNum;
+    EditText name;
+    EditText species;
+    EditText weight;
+    EditText height;
+    EditText hp;
+    EditText attack;
+    EditText defense;
+    //String upperString = myString.substring(0, 1).toUpperCase() + myString.substring(1).toLowerCase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
